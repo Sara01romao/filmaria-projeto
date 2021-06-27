a2537e19f7d5140c066650d82ea74a61e8ff954e
Sara Romao <sara-romao@live.com>
2021-06-26 22:35:57 -0300

    baseUrl e estrutura pg Home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,50 @@
 
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './home.css';
+
+import api from '../../services/api';
+
+
 export default function Home(){
+
+  const [filmes, setFilmes]= useState([]);
+
+
+   //funcão faz a chamada da api concatenando a baseurl com o resto da rotas
+  useEffect(() =>{
+
+   async function loadFilmes(){
+        const response = await api.get('r-api/?api=filmes/')
+        // console.log(response);
+
+        setFilmes(response.data);
+    }
+
+    loadFilmes();
+
+
+  }, []);
+
+ 
+
     return(
-      <div>
-          <h1>sara</h1>
+      <div className="container">
+          <div className="lista-filmes">
+              {filmes.map((filme)=>{
+                return(
+                  <article key={filme.id}>
+                    <strong>{filme.nome}</strong>
+
+                    <img src={filme.foto} alt={filme.nome}/>
+
+                    <Link to="/">Acessar</Link>
+
+
+                  </article>
+                )
+              })}
+          </div>
       </div>
     );
   }
---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+
+const api = axios.create({
+    baseURL: 'https://sujeitoprogramador.com/'
+})
+
+
+
+export default api;
+
+
+// base url  da rota da api que não vai mudar
+
+//link api: https://sujeitoprogramador.com/r-api/?api=filmes/
+
