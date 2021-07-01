b0fcc7fde3e1de6e309e7712c80876e1d6054b46
Sara Romao <sara-romao@live.com>
2021-06-30 21:14:59 -0300

    funcionalidade de add e deletar favoritos

--- src/pages/favoritos/index.js
+++ src/pages/favoritos/index.js
@@ -1,8 +1,50 @@
 
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import './favorito.css'
+
+
 export default function Favoritos(){
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+        const minhaLista = localStorage.getItem('filmes');
+        setFilmes(JSON.parse(minhaLista) || []);
+
+    }, []);
+
+
+    function handleDelete(id){
+        let filtroFilmes = filmes.filter((item)=>{
+            return(item.id !== id)
+        })
+
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem('filmes', JSON.stringify(filtroFilmes))
+    }
+
+
+
     return(
-        <div>
-            <h1>Pagina Filmes Favoritos</h1>
+        < div id="meus-filmes">
+            <h1>Meus Filmes</h1>
+
+            <ul>
+                {filmes.map((item)=>{
+                    return(
+                        <li key={item.id}>
+                            <span>{item.nome}</span>
+
+                            <div class="">
+                                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                                <button onClick={() => handleDelete(item.id)}>Excluir</button>
+
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
         
